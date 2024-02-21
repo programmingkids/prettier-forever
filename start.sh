e4031cb110c6cc88529547c024b015b47474c1a4
Hiroshi Fujiwara <fujiwara@no1s.biz>
2024-02-21 06:53:25 +0000

    update to checkout process

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
-cd /home/ec2-user/environment/prettier-forever
-npm start
+ps=`ps aux | grep prettier-forever/watch.js | grep -v grep | wc -l`
+
+if [ $ps -eq 0 ]; then
+	# 対象プロセスが存在しない場合
+	cd /home/ec2-user/environment/prettier-forever
+	npm start
+fi
 
 exit 0
